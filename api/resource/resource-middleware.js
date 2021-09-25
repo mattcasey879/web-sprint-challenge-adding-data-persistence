0d0f34cb0d69e726171cc3464b51938bc77c3404
Matthew Casey <mattcasey879@gmail.com>
2021-09-25 15:22:55 -0500

    one test left to go to pass

--- api/resource/resource-middleware.js
+++ api/resource/resource-middleware.js
@@ -1,24 +1,29 @@
-const express = require("express")
-const db = require("../../data/dbConfig")
+const e = require("express");
+const express = require("express");
+const db = require("../../data/dbConfig");
 
+const validateResourceNameUnique = async (req, res, next) => {
+    try {
+        const exsisting = await db("resources")
+            .where("resource_name", req.body.resource_name)
+            .first()
 
-const validateResourceNameUnique = (req, res, next) => {
-    const existing = db("resources").where("resource_name", req.body.resource_name)
-    if (existing === undefined) {
-        next({ staus: 404, message: "resource with that name already exists!"})
-    }
-    else{
-        next()
+            if (exsisting) {
+                next({ status: 400, message: "name is taken"})
+            } else {
+                next()
+            }
+    } catch (error) {
+        next(error)
     }
 }
-
 const validateResourceName = (req, res, next) => {
-    const {resource_name} = req.body
-    if(resource_name === undefined) {
-        next({ status: 404, message: "please enter a resource name"})
-    } else {
-        next()
-    }
-}
+  const { resource_name } = req.body;
+  if (resource_name === undefined) {
+    next({ status: 404, message: "please enter a resource name" });
+  } else {
+    next();
+  }
+};
 
-module.exports = { validateResourceName, validateResourceNameUnique }
+module.exports = { validateResourceName, validateResourceNameUnique };
